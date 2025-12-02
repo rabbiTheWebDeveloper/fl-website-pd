e0e4b8c02aee764bf8312117d701b3a546045313
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-12-02 16:42:12 +0600

    Update page.js

--- app/(multipage)/[shopName]/page.js
+++ app/(multipage)/[shopName]/page.js
@@ -4,11 +4,11 @@ import Home from "../_component/Home";
 const page = async ({ params }) => {
   const { shopName } = await params;
   const shopInfo = await getData(shopName);
-  return (
-    <>
-      <Home shopName={shopName} shopInfo={shopInfo} />
-    </>
-  );
+  if (!shopInfo) {
+    return <div>Shop not found</div>;
+  }
+
+  return <Home shopName={shopName} shopInfo={shopInfo} />;
 };
 
 export default page;
