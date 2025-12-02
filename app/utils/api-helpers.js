060a78a1409881e5fb227586dd8018d209e38701
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-12-02 16:45:39 +0600

    Update api-helpers.js

--- app/utils/api-helpers.js
+++ app/utils/api-helpers.js
@@ -1,49 +1,36 @@
 import { API_ENDPOINTS } from "../../config/ApiEndpoints";
 
 export const getData = async (shopName) => {
-  try {
-    const domainRes = await fetch(
-      `${API_ENDPOINTS.BASE_URL}${API_ENDPOINTS.SHOP.INFO}`,
-      {
-        headers: { domain: shopName },
-        cache: "no-store",
-      }
-    );
+  const domainRes = await fetch(
+    `${API_ENDPOINTS.BASE_URL}${API_ENDPOINTS.SHOP.INFO}`,
+    {
+      headers: { domain: shopName },
+      cache: "no-store",
+    }
+  );
 
-    if (!domainRes.ok) throw new Error("Failed to fetch domain info");
-
-    const { data: domainInfo } = await domainRes.json();
-
-    const [sliderRes, bannerRes] = await Promise.all([
-      fetch(`${API_ENDPOINTS.BASE_URL}/shops/content?type=slider`, {
-        headers: { "shop-id": domainInfo?.shop_id },
-        cache: "no-store",
-      }),
-      fetch(`${API_ENDPOINTS.BASE_URL}/shops/content?type=banner`, {
-        headers: { "shop-id": domainInfo?.shop_id },
-        cache: "no-store",
-      }),
-    ]);
-
-    const [sliderData, bannerData] = await Promise.all([
-      sliderRes.json(),
-      bannerRes.json(),
-    ]);
-
-    return {
-      ...domainInfo,
-      slider: sliderData?.data || [],
-      banner: bannerData?.data || [],
-    };
-  } catch (err) {
-    return {
-      theme_id: null,
-      slider: [],
-      banner: [],
-    };
-  }
+  if (!domainRes.ok) throw new Error("Failed to fetch domain info");
+  const { data: domainInfo } = await domainRes.json();
+  const [sliderRes, bannerRes] = await Promise.all([
+    fetch(`${API_ENDPOINTS.BASE_URL}/shops/content?type=slider`, {
+      headers: { "shop-id": domainInfo?.shop_id },
+      cache: "no-store",
+    }),
+    fetch(`${API_ENDPOINTS.BASE_URL}/shops/content?type=banner`, {
+      headers: { "shop-id": domainInfo?.shop_id },
+      cache: "no-store",
+    }),
+  ]);
+  const [sliderData, bannerData] = await Promise.all([
+    sliderRes.json(),
+    bannerRes.json(),
+  ]);
+  return {
+    ...domainInfo,
+    slider: sliderData?.data || [],
+    banner: bannerData?.data || [],
+  };
 };
-
 
 export const getOtherData = async (shopName, typeOfPage) => {
   const startTime = performance.now();
