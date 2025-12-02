2e923e7d3291829b6798445ca50ff21e3aea984a
S M AL RABBI <rabbithedeveloper@gmail.com>
2025-12-02 16:11:23 +0600

    Update page.js

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,13 +1,24 @@
 import { headers } from "next/headers";
 import { getData } from "../utils/api-helpers";
 import HomeComp from "./_component/HomeComp";
+export const metadata = {
+  title: "Funnel Liner - Automated Sales Funnel",
+  description: "Funnel Liner - Automated Sales Funnel",
+  icons: {
+    icon: "/images/favicon.png",
+  },
+  viewport: "width=device-width, user-scalable=no",
+  other: {
+    "facebook-domain-verification": "o2jyunge4d5l7n767p8yf4qkuz9j4b",
+  },
+};
 
 const page = async () => {
   const headerList = await headers();
   const host = headerList.get("host");
   const cleanDomain = host.replace(/^www\./, "");
   const data = await getData(cleanDomain);
-    console.log("hello ", data);
+  console.log("hello ", data);
   return (
     <>
       <HomeComp domainInfo={data} />
